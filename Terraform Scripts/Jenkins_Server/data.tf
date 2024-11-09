96f336cf5ab97fc82e2c980dc9da5205ebb6e13a
varad <varathu09@gmail.com>
2024-11-09 11:52:33 +0530

    fix error

--- Terraform Scripts/Jenkins_Server/data.tf
+++ Terraform Scripts/Jenkins_Server/data.tf
@@ -1,13 +1,19 @@
-data "aws_ami" "amazonlinux2" {
+data "aws_ami" "amazon_linux_2023" {
   most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["al2023-ami-2023.*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
--- Terraform Scripts/Jenkins_Server/main.tf
+++ Terraform Scripts/Jenkins_Server/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "JenkinsServer" {
-  ami                    = data.aws_ami.amazonlinux2.id
+  ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = var.my_instance_type
   key_name               = var.my_key
   vpc_security_group_ids = [aws_security_group.web-traffic.id]
